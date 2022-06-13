bf9084ae98feb1b799ef031c66f552764d41804a
Ryan Murphy <40311961+ryanmurphy42@users.noreply.github.com>
2022-06-13 16:02:32 +1000

    Delete function_reoxygenation_alpha.m

--- 2Code/function_reoxygenation_alpha.m
+++
@@ -1,5 +0,0 @@
-function out = function_reoxygenation_alpha(t,alpha_hypoxia,alpha_normoxia,tau_alpha,t_s)
-
-out = alpha_normoxia  + (alpha_hypoxia - alpha_normoxia).*exp(-(1./tau_alpha).*(t - t_s));
-
-end
