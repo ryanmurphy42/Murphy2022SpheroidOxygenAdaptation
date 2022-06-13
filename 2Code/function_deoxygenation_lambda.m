07e93c29124ddfd44f60c52b0d4909ba291f7966
Ryan Murphy <40311961+ryanmurphy42@users.noreply.github.com>
2022-06-13 16:01:47 +1000

    Delete function_deoxygenation_lambda.m

--- 2Code/function_deoxygenation_lambda.m
+++
@@ -1,5 +0,0 @@
-function out = function_deoxygenation_lambda(t,lambda_hypoxia,lambda_normoxia,tau_lambda,t_s)
-
-out = lambda_hypoxia + (lambda_normoxia - lambda_hypoxia).*exp(-(1./tau_lambda).*(t - t_s));
-
-end
